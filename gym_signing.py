9f1e9285f68d7c63522870ad7c92f3483ee96ca1
Dominik Trochowski <dominik.trochowski@gmail.com>
2023-02-23 22:51:54 +0100

    Refactr of gym_signing.py tool

--- gym_signing.py
+++ gym_signing.py
@@ -1,55 +1,49 @@
 #!/usr/bin/env python3
 
-import time
+from time import sleep
 import os
 import re
 import signal
 import sys
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from browsing_automations import BrowserAutomation
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
 
 # ---------------------------------------------------------------------------- #
 #                                Script settings                               #
 # ---------------------------------------------------------------------------- #
 
-SCRIPT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+HEADLESS = True
+COMMAND_EXECUTOR = 'http://192.168.1.131:3000/webdriver'
 
-COMMAND_EXECUTOR = '127.0.0.1:9515'
+TRENING = "Trening.Obwodowy"
+TRENER = "Małgorzata.Olejniczak"
+
 ZDROFIT_SITE = 'https://zdrofit.pl/#logowanie'
 ALCHEMIA_GYM_SCHEDULE_SITE = 'https://zdrofit.pl/kluby-fitness/gdansk-alchemia/grafik-zajec'
 
-HEADLESS = True
-
-TRENING = "Salsation"
-TRENER = "Dorota.Turzańska-Skiba"
-
 email = "dominik.trochowski@gmail.com"
+SCRIPT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 PASSWD_FILE_PATH = os.path.join(SCRIPT_DIR_PATH, os.pardir, "passwd_zdrofit")
 
 # ---------------------------------------------------------------------------- #
 
+# ---------------------------------------------------------------------------- #
+#                                    Helpers                                   #
+# ---------------------------------------------------------------------------- #
+
+def timeout_handler(signum, frame):
+    print("TIMEOUT occurred!")
+    raise Exception("TIMEOUT - while searching for training")
+
+
 def get_passwd():
     with open(PASSWD_FILE_PATH, 'r') as passwd_file:
         return passwd_file.read()
-
-
-def setup_browser(headless=False):
-    '''
-    Local Chromium Driver can be downaloaded from:
-    https://chromedriver.storage.googleapis.com/index.html?path=110.0.5481.77/
-    '''
-    chrome_options = webdriver.ChromeOptions()
-    if headless:
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-    return webdriver.Remote(command_executor=COMMAND_EXECUTOR, options=chrome_options)
 
 
 def save_screen(browser, file_name):
@@ -65,11 +59,6 @@ def find_training_id(browser, trening, trener):
     return re.findall('id=\"([a-z][0-9]{6})\"', training_descrićption.group(0))[0]
 
 
-def timeout_handler(signum, frame):
-    print("TIMEOUR occured!")
-    raise Exception("TIMEOUT - while searching for training")
-
-
 def try_to_click(browser, training_id):
     try:
         browser.find_element(By.ID, training_id).click()
@@ -81,7 +70,7 @@ def try_to_click(browser, training_id):
 
 def get_into_training_signing(browser, training_id):
     FUNCTION_TIMEOUT = 20 # s
-    SCROLL_PX_ITER = 500 # px
+    SCROLL_ITER_PX = 500 # px
     
     signal.signal(signal.SIGALRM, timeout_handler)
     signal.alarm(FUNCTION_TIMEOUT)
@@ -91,53 +80,59 @@ def get_into_training_signing(browser, training_id):
 
     while (not try_to_click(browser, training_id)):
         browser.execute_script(f"window.scrollTo({x_pos}, {y_pos})")
-        y_pos += SCROLL_PX_ITER
-        time.sleep(0.2)
+        y_pos += SCROLL_ITER_PX
+        sleep(0.2)
 
     signal.alarm(0)
 
+# ---------------------------------------------------------------------------- #
 
-def scrap():
-    browser = setup_browser(HEADLESS)
+# ---------------------------------------------------------------------------- #
+#                               Automation Steps                               #
+# ---------------------------------------------------------------------------- #
 
-    print("[1] Open login page")
-    browser.get(ZDROFIT_SITE)
-    time.sleep(2)
+def accept_cookies(workspace, browser):
+    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'didomi-notice-agree-button'))).click()
 
-    print("[2] Accept cookies")
-    if not HEADLESS:
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'didomi-notice-agree-button'))).click()
-        time.sleep(2)
 
-    print("[3] Enter credentials")
+def enter_credentials(workspace, browser):
     browser.find_element(By.ID, 'member_login_form_email').send_keys(email)
     browser.find_element(By.ID, 'member_login_form_password').send_keys(get_passwd())
     browser.find_element(By.ID, 'member_login_form_submit').click()
-    time.sleep(1)
 
-    print("[4] Jump to gym schedule")
+
+def jump_to_gym_schedule(workspace, browser):
     browser.get(ALCHEMIA_GYM_SCHEDULE_SITE)
-    time.sleep(1)
 
-    print("[5] Find the right training")
+
+def find_right_trenning(workspace, browser):
     training_id = find_training_id(browser, TRENING, TRENER)
-    time.sleep(1)
+    workspace["training_id"] = training_id
 
-    print("[6] Click on chosen training")
+
+def click_on_chosen_training(workspace, browser):
     try:
-        get_into_training_signing(browser, training_id)
+        get_into_training_signing(browser, workspace["training_id"])
     except Exception as e:
         print(e)
         sys.exit(-1)
-    time.sleep(1)
+        
 
-    print("[7] Sign up for training")
+def training_sign_up(workspace, browser):
     WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'schedule_register_form_submit'))).click()
-    time.sleep(2)
-    
-    if HEADLESS:
-        browser.quit()
 
+# ---------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
-    scrap()
+    automation = BrowserAutomation(COMMAND_EXECUTOR, ZDROFIT_SITE, HEADLESS)
+    
+    automation.add_step(accept_cookies, "Accept cookies", wait_after_in_sec=1.5, in_headless=False)
+    automation.add_step(enter_credentials, "Enter credentials", wait_after_in_sec=1, in_headless=True)
+    automation.add_step(jump_to_gym_schedule, "Jump to gym schedule", wait_after_in_sec=1, in_headless=True)
+    automation.add_step(find_right_trenning, "Find the right training", wait_after_in_sec=0, in_headless=True)
+    automation.add_step(click_on_chosen_training, "Click on chosen training", wait_after_in_sec=1.5, in_headless=True)
+    automation.add_step(training_sign_up, "Sign up for training", wait_after_in_sec=1.5, in_headless=True)
+
+    automation.perform_all_steps()
+
+    del automation
